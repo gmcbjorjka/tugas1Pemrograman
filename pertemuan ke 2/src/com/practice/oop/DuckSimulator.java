9ba99a14c8bf04f045d6fb71d0dbc1f1279f12c6
gmc bjorjka <117149089+gmcbjorjka@users.noreply.github.com>
2023-10-01 11:44:09 +0700

    DuckSimulator.java

--- pertemuan ke 2/src/com/practice/oop/DuckSimulator.java
+++ pertemuan ke 2/src/com/practice/oop/DuckSimulator.java
@@ -9,22 +9,55 @@ package com.practice.oop;
  * @author teguh
  */
 public class DuckSimulator {
-    public static void main (String[] args){
-        MallardDuck mallard = new MallardDuck();
-        FlyBehavior cantFly = ()-> System.out.println("I'm can't fly");
-        QuackBehavior squeak = ()-> System.out.println("squeak");
-        RubberDuck rubberDuckie = new RubberDuck(cantFly,squeak);
-        DecoyDuck decoy = new DecoyDuck();
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         
-        Duck model = new ModelDuck();
+        System.out.println("Pilih jenis Duck:");
+        System.out.println("1. Mallard Duck");
+        System.out.println("2. Rubber Duck");
+        System.out.println("3. Decoy Duck");
+        System.out.println("4. Model Duck");
+        System.out.print("Masukkan nomor pilihan Anda: ");
         
-        mallard.performQuack();
-        rubberDuckie.performQuack();
-        decoy.performQuack();
+        int choice = scanner.nextInt();
+        Duck selectedDuck = null;
+
+        switch (choice) {
+            case 1:
+                selectedDuck = new MallardDuck();
+                break;
+            case 2:
+                FlyBehavior cantFly = () -> System.out.println("I can't fly");
+                QuackBehavior squeak = () -> System.out.println("squeak");
+                selectedDuck = new RubberDuck(cantFly, squeak);
+                break;
+            case 3:
+                selectedDuck = new DecoyDuck();
+                break;
+            case 4:
+                selectedDuck = new ModelDuck();
+                break;
+            default:
+                System.out.println("Pilihan tidak valid.");
+                return;
+        }
         
-        model.performFly();
-        model.setFlyBehavior(new FlyRocketPowered() );
-        model.performFly();
+        System.out.println("Anda telah memilih " + selectedDuck.getClass().getSimpleName());
+        
+        selectedDuck.performQuack();
+        
+        if (selectedDuck instanceof ModelDuck) {
+            System.out.println("Anda juga dapat mengubah perilaku terbang Model Duck.");
+            System.out.println("Apakah Anda ingin Model Duck terbang dengan roket? (ya/tidak)");
+            String response = scanner.next();
+            if (response.equalsIgnoreCase("ya")) {
+                ((ModelDuck) selectedDuck).setFlyBehavior(new FlyRocketPowered());
+                selectedDuck.performFly();
+            }
+        } else {
+            selectedDuck.performFly();
+        }
+        
+        scanner.close();
     }
-    
 }
